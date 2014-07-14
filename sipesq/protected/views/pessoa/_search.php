9cd3ae1d2c968a5fb96120060e9a1d9e335db316
Guilherme Severo <grsevero@gmail.com>
2014-07-14 14:30:18 -0300

    updates gerais

--- sipesq/protected/views/pessoa/_search.php
+++ sipesq/protected/views/pessoa/_search.php
@@ -6,11 +6,6 @@
 )); ?>
 
 	<div class="row">
-		<?php echo $form->label($model,'cod_pessoa'); ?>
-		<?php echo $form->textField($model,'cod_pessoa'); ?>
-	</div>
-
-	<div class="row">
 		<?php echo $form->label($model,'nome'); ?>
 		<?php echo $form->textField($model,'nome'); ?>
 	</div>
@@ -25,15 +20,6 @@
 		<?php echo $form->textField($model,'telefone'); ?>
 	</div>
 
-	<div class="row">
-		<?php echo $form->label($model,'cpf'); ?>
-		<?php echo $form->textField($model,'cpf'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'rg'); ?>
-		<?php echo $form->textField($model,'rg'); ?>
-	</div>
 
 	<div class="row">
 		<?php echo $form->label($model,'cartao_ufrgs'); ?>
@@ -41,72 +27,12 @@
 	</div>
 
 	<div class="row">
-		<?php echo $form->label($model,'cod_projeto_atuante'); ?>
-		<?php echo $form->textField($model,'cod_projeto_atuante'); ?>
-	</div>
-
-	<div class="row">
 		<?php echo $form->label($model,'email'); ?>
 		<?php echo $form->textField($model,'email'); ?>
 	</div>
 
-	<div class="row">
-		<?php echo $form->label($model,'cidade'); ?>
-		<?php echo $form->textField($model,'cidade'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'rua_complemento'); ?>
-		<?php echo $form->textField($model,'rua_complemento'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'bairro'); ?>
-		<?php echo $form->textField($model,'bairro'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'cep'); ?>
-		<?php echo $form->textField($model,'cep'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'banco'); ?>
-		<?php echo $form->textField($model,'banco'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'agencia'); ?>
-		<?php echo $form->textField($model,'agencia'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'conta_corrente'); ?>
-		<?php echo $form->textField($model,'conta_corrente'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'lattes'); ?>
-		<?php echo $form->textField($model,'lattes'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'data_nascimento'); ?>
-		<?php echo $form->textField($model,'data_nascimento'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'cod_vinculo_institucional'); ?>
-		<?php echo $form->textField($model,'cod_vinculo_institucional'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'cod_banco'); ?>
-		<?php echo $form->textField($model,'cod_banco'); ?>
-	</div>
-
 	<div class="row buttons">
-		<?php echo CHtml::submitButton('Search'); ?>
+		<?php echo CHtml::submitButton('Buscar'); ?>
 	</div>
 
 <?php $this->endWidget(); ?>
